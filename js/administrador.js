57e76a66c52a601c4942149e0693e3de4b8519fd
Ezequiel Romeo <ezequiel.romeo@gmail.com>
2024-06-20 15:03:15 -0300

    administrador - ACCIONES

--- js/administrador.js
+++ js/administrador.js
@@ -232,4 +232,347 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    const expandReduceIcon = document.getElementById('expandReduceIcon');
+    const offcanvas = document.querySelector('.offcanvas.offcanvas-end');
+    const navTabs = document.querySelector('.nav.nav-tabs');
+    const formAcciones = document.querySelector('.formAcciones');
+    const formSuperior = document.querySelector('.formSuperior');
+    const formInferior = document.querySelector('.formInferior');
+
+    expandReduceIcon.addEventListener('click', function () {
+        if (offcanvas.style.width === '100%') {
+            offcanvas.style.width = '50%';
+            expandReduceIcon.src = '../img/expandIcon.svg';
+            navTabs.classList.remove('d-flex', 'align-items-center', 'justify-content-between');
+            formAcciones.classList.remove('d-flex');
+            formSuperior.style.width = '100%';
+            formInferior.style.width = '100%';
+        } else {
+            offcanvas.style.width = '100%';
+            expandReduceIcon.src = '../img/reduceIcon.svg';
+            navTabs.classList.add('d-flex', 'align-items-center', 'justify-content-between');
+            formAcciones.classList.add('d-flex');
+            formSuperior.style.width = '50%';
+            formInferior.style.width = '50%';
+            formSuperior.style.float = 'left';
+            formInferior.style.float = 'left';
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const urlSelect = document.getElementById('urlSelect');
+    const urlModal = new bootstrap.Modal(document.getElementById('urlModal'), {
+        keyboard: true
+    });
+
+    urlSelect.addEventListener('click', function() {
+        urlModal.show();
+    });
+});
+
+// Modal Url
+
+document.addEventListener('DOMContentLoaded', function() {
+    const datos = [
+        {
+            accionFormativa: "A.F.:35654 - THEPOWERMBA",
+            grupo: "Grupo 699",
+            fechaInicio: "22/03/2023",
+            fechaFinalizacion: "22/03/2023",
+            modalidad: "teleformación",
+            acceso: "Acceso a plataforma",
+            informe: "Ver informe"
+        },
+        {
+            accionFormativa: "A.F.:35690 - THEPOWERDIGITALMARKETING",
+            grupo: "Grupo 701",
+            fechaInicio: "22/03/2023",
+            fechaFinalizacion: "22/03/2023",
+            modalidad: "Aul@ Virtual",
+            acceso: "Acceso a plataforma",
+            informe: "Ver informe"
+        },
+        {
+            accionFormativa: "A.F.:35700 - FUTURELEADERS",
+            grupo: "Grupo 706",
+            fechaInicio: "22/03/2023",
+            fechaFinalizacion: "22/03/2023",
+            modalidad: "teleformación",
+            acceso: "Acceso a plataforma",
+            informe: "Ver informe"
+        },
+        {
+            accionFormativa: "A.F.:66491- TECNOLOGÍAS DEL LENGUAJE PARA DESARROLLO DE INTERFACES CONVERSACIONALES",
+            grupo: "Grupo 1561",
+            fechaInicio: "22/03/2023",
+            fechaFinalizacion: "22/03/2023",
+            modalidad: "teleformación",
+            acceso: "Acceso a plataforma",
+            informe: "Ver informe"
+        }
+    ];
+
+    const datosTeleformacion = [
+        {
+            alumno: "Hernández Santiago",
+            asistencia: "28,56%",
+            tipoDiploma: "Aprovechas",
+            certifica: "Si",
+            contenidos: "100%",
+            tiempoTotal1: "2.3 Horas",
+            eval: "Si",
+            mensajes: "2",
+            tiempoTotal2: "0.2 Horas",
+            acceso: "Si",
+            primera1: "02/08/2023 10:11",
+            ultima1: "12/08/2023 14:33",
+            total: "33",
+            primera2: "02/08/2023 10:11",
+            ultima2: "12/08/2023 14:33",
+            resultado: "100% Passed"
+        },
+        {
+            alumno: "Rodríguez Matthias",
+            asistencia: "28,56%",
+            tipoDiploma: "",
+            certifica: "",
+            contenidos: "No",
+            tiempoTotal1: "",
+            eval: "",
+            mensajes: "No",
+            tiempoTotal2: "",
+            acceso: "",
+            primera1: "No",
+            ultima1: "",
+            total: "",
+            primera2: "No",
+            ultima2: "",
+            resultado: ""
+        },
+        {
+            alumno: "González María",
+            asistencia: "28,56%",
+            tipoDiploma: "",
+            certifica: "",
+            contenidos: "No",
+            tiempoTotal1: "",
+            eval: "",
+            mensajes: "No",
+            tiempoTotal2: "",
+            acceso: "",
+            primera1: "No",
+            ultima1: "",
+            total: "",
+            primera2: "No",
+            ultima2: "",
+            resultado: ""
+        },
+        {
+            alumno: "Alumnos: 3",
+            asistencia: "28,56%",
+            tipoDiploma: "",
+            certifica: "",
+            contenidos: "",
+            tiempoTotal1: "",
+            eval: "",
+            mensajes: "",
+            tiempoTotal2: "",
+            acceso: "",
+            primera1: "",
+            ultima1: "",
+            total: "",
+            primera2: "",
+            ultima2: "",
+            resultado: ""
+        }
+    ];
+
+    const datosAulaVirtual = [
+        {
+            alumno: "Diego Pérez 52984606N",
+            fechaAccesoSesion: "22/03/2023",
+            horaAcceso: "10:31:05",
+            firmaAcceso: "Si",
+            firmaDeclaracion: "-",
+            tiempoConectadoSesion: "29 min.",
+            porcentajeAsistenciaTotalCurso: "50%"
+        },
+        {
+            alumno: "Luis Pérez 52987564B",
+            fechaAccesoSesion: "22/03/2023",
+            horaAcceso: "10:30:15",
+            firmaAcceso: "Si",
+            firmaDeclaracion: "-",
+            tiempoConectadoSesion: "29 min.",
+            porcentajeAsistenciaTotalCurso: "100%"
+        },
+        {
+            alumno: "Rita Gómez 98765436Y",
+            fechaAccesoSesion: "22/03/2023",
+            horaAcceso: "10:31:21",
+            firmaAcceso: "Si",
+            firmaDeclaracion: "-",
+            tiempoConectadoSesion: "29 min.",
+            porcentajeAsistenciaTotalCurso: "100%"
+        },
+        {
+            alumno: "Gabriel García Y98654775",
+            fechaAccesoSesion: "22/03/2023",
+            horaAcceso: "-",
+            firmaAcceso: "-",
+            firmaDeclaracion: "Si",
+            tiempoConectadoSesion: "-",
+            porcentajeAsistenciaTotalCurso: "100%"
+        }
+    ];
+    
+    
+
+    const tableBodyEjecucion = document.getElementById('tableBodyEjecucion');
+    const tableBodyFinalizadas = document.getElementById('tableBodyFinalizadas');
+    const tableBodyTeleformacion = document.getElementById('tableBodyTeleformacion');
+    const tableBodyAulaVirtual = document.getElementById('tableBodyAulaVirtual');
+    
+    let rows = '';
+    datos.forEach(d => {
+        rows += `<tr>
+            <td class="accionFormativa">${d.accionFormativa}</td>
+            <td class="grupo">${d.grupo}</td>
+            <td class="fechaInicio">${d.fechaInicio}</td>
+            <td class="fechaFinal">${d.fechaFinalizacion}</td>
+            <td class="modalidad">${d.modalidad}</td>
+            <td class="acceso"><a>${d.acceso}</a></td>
+            <td class="informe"><a>${d.informe}</a></td>
+        </tr>`;
+    });
+    tableBodyEjecucion.innerHTML = rows;
+    tableBodyFinalizadas.innerHTML = rows;
+
+    let rowsTeleformacion = '';
+    datosTeleformacion.forEach(d => {
+        rowsTeleformacion += `<tr>
+            <td>${d.alumno}</td>
+            <td>${d.asistencia}</td>
+            <td>${d.tipoDiploma}</td>
+            <td>${d.certifica}</td>
+            <td class="${d.contenidos === '100%' ? 'colorVerde' : (d.contenidos === 'No' ? 'colorRojo' : '')}">${d.contenidos}</td>
+            <td class="colorNegro">${d.tiempoTotal1}</td>
+            <td class="${d.eval === 'Si' ? 'colorVerde' : (d.eval === 'No' ? 'colorRojo' : '')}">${d.eval}</td>
+            <td class="${d.mensajes === '2' ? 'colorVerde' : (d.mensajes === 'No' ? 'colorRojo' : '')}">${d.mensajes}</td>
+            <td class="colorNegro">${d.tiempoTotal2}</td>
+            <td class="colorNegro">${d.acceso}</td>
+            <td class="${d.primera1 === 'No' ? 'colorRojo' : ''}">${d.primera1}</td>
+            <td class="colorNegro">${d.ultima1}</td>
+            <td class="colorNegro">${d.total}</td>
+            <td class="colorNegro">${d.primera2}</td>
+            <td class="colorNegro">${d.ultima2}</td>
+            <td class="${d.resultado === '100% Passed' ? 'passed' : ''}">${d.resultado}</td>
+        </tr>`;
+    });
+    tableBodyTeleformacion.innerHTML = rowsTeleformacion;
+
+    let rowsAulaVirtual = '';
+    datosAulaVirtual.forEach(d => {
+        rowsAulaVirtual += `<tr>
+        <td class="colorGris">${d.alumno}</td>
+        <td class="colorGris">${d.fechaAccesoSesion}</td>
+        <td class="colorGris">${d.horaAcceso}</td>
+        <td class="colorGris">${d.firmaAcceso}</td>
+        <td class="${d.firmaDeclaracion === 'Si' ? 'colorVerde clickable' : ''}" onclick="${d.firmaDeclaracion === 'Si' ? 'openModal()' : ''}">${d.firmaDeclaracion}</td>
+        <td class="colorGris">${d.tiempoConectadoSesion}</td>
+        <td class="${d.porcentajeAsistenciaTotalCurso === '100%' ? 'colorVerde' : (d.porcentajeAsistenciaTotalCurso === '50%' ? 'colorRojo' : '')}">${d.porcentajeAsistenciaTotalCurso}</td>
+    </tr>
+    `;
+    })
+
+    tableBodyAulaVirtual.innerHTML = rowsAulaVirtual;
+
+    tableBodyEjecucion.addEventListener('click', function(e) {
+        if (e.target.tagName === 'A' && e.target.innerText === "Ver informe") {
+            const modalidad = e.target.closest('tr').querySelector('.modalidad').innerText;
+            let modalId;
+
+            if (modalidad === 'teleformación') {
+                modalId = '#modalTeleformacion';
+            } else if (modalidad === 'Aul@ Virtual') {
+                modalId = '#modalAulaVirtual';
+            }
+
+            if (modalId) {
+                const modal = new bootstrap.Modal(document.querySelector(modalId), {
+                    keyboard: true
+                });
+                modal.show();
+            }
+        }
+    });
+
+    tableBodyFinalizadas.addEventListener('click', function(e) {
+        if (e.target.tagName === 'A' && e.target.innerText === "Ver informe") {
+            const modalidad = e.target.closest('tr').querySelector('.modalidad').innerText;
+            let modalId;
+
+            if (modalidad === 'teleformación') {
+                modalId = '#modalTeleformacion';
+            } else if (modalidad === 'Aul@ Virtual') {
+                modalId = '#modalAulaVirtual';
+            }
+
+            if (modalId) {
+                const modal = new bootstrap.Modal(document.querySelector(modalId), {
+                    keyboard: true
+                });
+                modal.show();
+            }
+        }
+    });
+});
+
+
+function openModal() {
+    var myModal = new bootstrap.Modal(document.getElementById('modalDeclaracionResponsable'), {
+        keyboard: true
+    });
+    myModal.show();
+}
+
+function setupDragToScroll(element) {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    element.addEventListener('mousedown', (e) => {
+        isDown = true;
+        element.style.cursor = 'grabbing';
+        startX = e.pageX - element.offsetLeft;
+        scrollLeft = element.scrollLeft;
+    });
+
+    element.addEventListener('mouseleave', () => {
+        isDown = false;
+        element.style.cursor = 'grab';
+    });
+
+    element.addEventListener('mouseup', () => {
+        isDown = false;
+        element.style.cursor = 'grab';
+    });
+
+    element.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - element.offsetLeft;
+        const walk = (x - startX) * 2; 
+        element.scrollLeft = scrollLeft - walk;
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navElement = document.querySelector('.nav-wrapper');
+    setupDragToScroll(navElement);
+});
 
